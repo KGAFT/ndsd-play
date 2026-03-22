2ae605f97476967bd3a7df4356e4abce0265bc93
kgaft <your_email@example.com>
2026-03-22 14:21:33 +0300

    Project initialized

---
+++ README.md
@@ -0,0 +1,44 @@
+# ndsd-play
+A lightweight player in rust for native dsd playback over compatible devices.
+This player will support only dsd without pcm conversion, you will need dac with dsd support!
+
+
+# Currently supported:
+
+
+Feature | status
+--- | --- 
+dsf/dsdiff reading | supported
+dsd playback | supported
+metadata parsing | TODO
+
+
+# If you struggle to build on windows
+
+Modify the existing visual studio installation to support desktop development and linux one
+
+Install the LLVM prebuild binaries, download it from the llvm project github repo.
+Set LIBCLANG_PATH system environment variable pointing to the root of llvm/bin. E.g: C:\clang+llvm-22.1.0-x86_64-pc-windows-msvc\bin.
+Also pass this directory to the system PATH variable.
+
+If you have problems with function ASIOSetSampleRate and ASIOGetSampleRate it is an msvc bug.
+Download and install ASIO SDK to your windows machine
+Create the environment variable in the system space/userspace "CPAL_ASIO_DIR" pointing to the root of sdk
+
+Go to your asio sdk, find asio.h and replace this:
+
+```
+#if IEEE754_64FLOAT
+	typedef double ASIOSampleRate;
+#else
+	typedef struct ASIOSampleRate {
+		char ieee[8];
+	} ASIOSampleRate;
+#endif
+```
+
+with this:
+
+```
+typedef double ASIOSampleRate;
+```****
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+use clap::{Parser, Subcommand};
+use ndsdplayback::players::{create_player, enumerate_supported_devices};
+use std::time::Duration;
+use tokio::time::sleep;
+
+
+///ndsd-play is a tool to play dsd file through sinks with support of native dsd
+/// To play a file you need to get your device id firstly, use list-devices command and remember id of your device
+///
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Command {
+    ///Enumerate supported devices in system
+    ListDevices,
+    ///Play a file through(device_id device_id can be obtained from list-devices command)
+    /// Example: ndsd-play open <device_id> <path_to_file>
+    /// Real world example: ndsd-play open 1 "/mnt/hdd/Music/Alphaville - Forever Young (Remastered) (1984_2019) [LP] DSD128/Alphaville - Forever Young (Remastered) (1984_2019) [LP] DSD128.dff"
+    Open { device: u32, file: String },
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::ListDevices => {
+            let devices = enumerate_supported_devices();
+            println!("");
+            println!("");
+            println!("");
+
+            if devices.is_empty() {
+                println!("NO SUITABLE DEVICES FOUND");
+            } else {
+                println!("FOUND {} SUITABLE DEVICES", devices.len());
+                for i in 0..devices.len() {
+                    println!(
+                        "Id: {} Short name: {} Full name: {}",
+                        i,
+                        devices[i].0.to_string_lossy(),
+                        devices[i].1.to_string_lossy()
+                    );
+                }
+            }
+        }
+        Command::Open { device, file } => {
+            open_file_and_play(file, device).await;
+        }
+    }
+}
+
+async fn open_file_and_play(path: String, device: u32) {
+    let devices = enumerate_supported_devices();
+    if device >= devices.len() as u32 {
+        eprintln!("Device not found or busy");
+        return;
+    }
+
+    let player = create_player(devices[device as usize].0.clone());
+    if player.is_none() {
+        eprintln!("Failed to create player with current device");
+        return;
+    }
+    let mut player = player.unwrap();
+    player.load_new_track(path.as_str()).await;
+    player.start().await;
+    sleep(Duration::from_millis(2500)).await;
+    println!("Control playback by entering commands from the list below");
+    println!(
+        "Controls: (Pause/Play - p), (Seek - enter number in float like(0.1-0.9)), (Get current progress - e), (Stop - s)"
+    );
+    let mut paused = false;
+    while player.is_playing().await {
+        let mut input = String::new();
+        input = input.to_lowercase();
+        std::io::stdin().read_line(&mut input).expect("Failed");
+        let mut chars = input.chars();
+        match chars.nth(0).unwrap() {
+            'p' => {
+                if !paused {
+                    player.pause().await;
+                } else {
+                    player.play().await;
+                }
+                paused = !paused;
+            }
+            '0' => {
+                let seek: Result<f64, _> = input.trim().parse();
+                match seek {
+                    Ok(seek) => {
+                        if seek >= 0f64 && seek <= 1f64 {
+                            let res = player.seek(seek).await;
+                            if res.is_err() {
+                                eprintln!("Seek failed {}", res.unwrap_err());
+                            }
+                        } else {
+                            eprintln!("Invalid number entered");
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("Invalid number entered {} {}", err, input);
+                    }
+                }
+            }
+            'e' => {
+                println!("Progress: {}", player.get_pos().await);
+            }
+            's' => {
+                player.stop().await;
+                break;
+            }
+            _ => {
+                eprintln!("invalid command");
+            }
+        }
+    }
+    drop(player);
+}
